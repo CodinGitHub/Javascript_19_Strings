4585026aedf7994c1904301dff39bf6f72893061
CodinGitHub <gms2programador@gmail.com>
2021-09-20 04:58:09 -0500

    Agrendando estilos basicos

--- main.js
+++ main.js
@@ -4,4 +4,27 @@ let nombre1 = "Juan";
 let nombre2 = 'Pedro';
 let nombre3 = `Maria`;
 
-let frase = "Mi lenguaje favorito es "Javascript".";
+// let frase = "Mi lenguaje favorito es "Javascript"."
+
+// let frase = `Lista de compras:
+// * Aceite
+// * Arroz
+// * Carne
+// * Frutas`;
+
+// let frase = `Lista de compras:\n* Aceite \n* Arroz \n* Carne \n* Frutas`;
+
+let saludo = 'Hola Mundo!';
+// console.log(saludo[0]);
+// for(let i=0; i<saludo.length; i++){
+//    console.log(saludo[i]);
+// }
+
+// for(let iterador of 'Hola Mundo!'){
+//   console.log(iterador);
+// }
+
+saludo[0]='h';
+console.log(saludo[0]);
+
+CAMBIANDO MAYUSCULAS Y MINUSCULAS
