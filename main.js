d6fb2730114df6e59c3bdd98760b9b97cb5416bf
CodinGitHub <gms2programador@gmail.com>
2021-09-25 05:34:02 -0500

    Video grabado

--- main.js
+++ main.js
@@ -1,67 +1,63 @@
-// 3 Formas en las que se puede declarar un string
-
+// STRINGS
 let nombre1 = "Juan";
 let nombre2 = 'Pedro';
 let nombre3 = `Maria`;
 
-let declaracion = 'Mi lenguaje favorito es "Javascript".'
+let declaracion = "Mi lenguaje favorito es 'Javascript'."
 console.log(declaracion);
 
-let compras = `Lista de compras:
+let compras = `lista de compras:
 * Aceite
 * Arroz
 * Carne
 * Frutas`;
 console.log(compras);
 
-
-let compras2 = `Lista de compras:\n* Aceite \n* Arroz \n* Carne \n* Frutas`;
+let compras2 = "lista de compras: \n* Aceite \n* Arroz \n* Carne \n* Frutas";
 console.log(compras2);
 
-//Largo del string
+//Template strings
+let edad = 15;
+console.log(`Roberto tiene ${edad} años.`);
+console.log("Roberto tiene " + edad + " años.");
 
+//Largo del string
 let saludo = 'Hola Mundo!';
 console.log(saludo.length);
 
 //Accediendo a caracteres
+console.log(saludo[10]);
 
+saludo[0] = 'h';
 console.log(saludo[0]);
 
-//Los string son inmutables
-
-saludo[0]='h';
+saludo = 'hola Mundo!';
 console.log(saludo[0]);
 
-saludo = "hola Mundo!"
-console.log(saludo[0]);
-
-//Cambiando entre mayúsculas y minúsculas
+//Cambiando entre mayusculas y minusculas
 console.log(saludo.toUpperCase());
 console.log(saludo.toLowerCase());
 
 //Subcadenas
-
-let frase = 'HTML es un lenguaje de marcado y Javascript es un lenguaje de programación'
+let frase = 'HTML es un lenguaje de marcado y Javascript es un lenguaje de programacion';
 console.log(frase.indexOf('HTML'));
 console.log(frase.indexOf('lenguaje'));
-console.log(frase.indexOf('es'));
-
-console.log(frase.indexOf('es', 6));
+console.log(frase.indexOf('es',6));
 console.log(frase.indexOf('CSS'));
-
 console.log(frase.lastIndexOf('es'));
 
-//includes, startsWith, endsWith
-
+//includes, startsWith, endsWidth
 console.log(frase.includes('lenguaje'));
 console.log(frase.includes('CSS'));
-console.log(frase.startsWith('HTML'));
-console.log(frase.endsWith('programación'));
 console.log(frase.endsWith('programacion'));
+console.log(frase.endsWith('programación'));
 
-//Obteniendo un substring (substring, substr y slice)
+//Substrings
+let lenguaje = ' Javascript ';
+// lenguaje = lenguaje.trim();
+console.log(lenguaje.substring(2,4));
+console.log(lenguaje.substr(2,4));
 
-let lenguaje = 'Javascript';
-console.log(lenguaje.substring(2,5));
-console.log(lenguaje.substr(-5, 4));
-console.log(lenguaje.slice(-5,-3));
+//trim y repeat
+
+console.log(lenguaje.repeat(10));
